9e153329c0e0cf62d241e82eeff22c49d779923d
Arman Vardanyan <arman5991@mail.ru>
2017-11-09 16:38:07 +0400

    09.11.2017

--- Navigacia/app/src/main/java/com/navigacia/MapsActivity.java
+++ Navigacia/app/src/main/java/com/navigacia/MapsActivity.java
@@ -2,14 +2,19 @@ package com.navigacia;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,29 +22,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        DirectionFinderListener
-{
+        DirectionFinderListener, View.OnClickListener {
 
     private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
@@ -68,12 +81,66 @@ public class MapsActivity extends FragmentActivity implements
         etOrigin = (EditText) findViewById(R.id.etOrigin);
         etDestination = (EditText) findViewById(R.id.etDestination);
 
-        btnFindPath.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendRequest();
+        btnFindPath.setOnClickListener(this);
+        etOrigin.setOnClickListener(this);
+        etDestination.setOnClickListener(this);
+
+//        btnFindPath.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sendRequest();
+//                Uri intentUri = Uri.parse("google.navigation:ll=40.47838,44.31349");
+//                Intent routeIntent = new Intent(Intent.ACTION_VIEW, intentUri);
+//                routeIntent.setPackage("com.navitel");
+//                if (routeIntent.resolveActivity(getPackageManager()) == null) {
+//                    // Navitel не установлен, откроем страницу в маркете
+//                    Log.i("ssssssssssssss", "ssssssssssssssss");
+//                    routeIntent = new Intent(Intent.ACTION_VIEW);
+//                    routeIntent.setData(Uri.parse("market://details?id=com.navitel"));
+//                }
+//                startActivityForResult(routeIntent,0);
+//                Uri gmmIntentUri = Uri.parse("google.streetview:cbll=40.47838,44.31349");
+//                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+//                mapIntent.setPackage("com.google.android.apps.maps");
+//                startActivity(mapIntent);
+//            }
+//        });
+    }
+
+    private void autocomplit(int PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_GEOCODE)
+                .build();
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .setFilter(typeFilter)
+                    .build(MapsActivity.this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 0 || requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(MapsActivity.this, data);
+                Log.i("ssssssssss", String.valueOf(place));
+                if (requestCode == 0) {
+                    etOrigin.setText(place.getName());
+                } else etDestination.setText(place.getName());
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(MapsActivity.this, data);
+                // TODO: Handle the error.
+                Log.i("ssssssssss", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
             }
-        });
+        }
+
     }
 
     private void sendRequest() {
@@ -159,7 +226,7 @@ public class MapsActivity extends FragmentActivity implements
         }
 
         if (polylinePaths != null) {
-            for (Polyline polyline:polylinePaths ) {
+            for (Polyline polyline : polylinePaths) {
                 polyline.remove();
             }
         }
@@ -195,6 +262,24 @@ public class MapsActivity extends FragmentActivity implements
                 polylineOptions.add(route.points.get(i));
 
             polylinePaths.add(mMap.addPolyline(polylineOptions));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnFindPath: {
+                sendRequest();
+                break;
+            }
+            case R.id.etOrigin: {
+                autocomplit(0);
+                break;
+            }
+            case R.id.etDestination: {
+                autocomplit(1);
+                break;
+            }
         }
     }
 }
